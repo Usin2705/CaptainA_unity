a448194d8ae5a578b28895ab99243e8b8df10e6a
Nhan Phan <nhanphan2705@gmail.com>
2023-05-19 00:15:28 +0300

    BUG: avoid record the notifi sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -28,11 +28,11 @@ public class AudioManager : MonoBehaviour
     public void StartRecording()
     {
         //Debug.Log("Play record notif");
-        audioSource.PlayOneShot(recordNotif, 0.8f);
+        audioSource.PlayOneShot(recordNotif, 0.5f);
         //The notification sound cause some noise in the recording
         //So we need to delay it a little
-        //No need to wait for the sound to finish playing
-        //Invoke(nameof(RecordSound), Const.BUFFER_TIME);
+        //The new notification sound is just 0.3f long
+        Invoke(nameof(RecordSound), 0.31f);
         RecordSound();
     }
 
