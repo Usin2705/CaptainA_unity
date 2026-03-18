73ae409364935b80433f2b2a0df0af5317c905a2
AaroMKS <aaro.savolainen@helsinki.fi>
2026-03-18 17:20:07 +0200

    Self review grade is saved

--- Assets/Scripts/AdvancePanel.cs
+++ Assets/Scripts/AdvancePanel.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
+
 public class AdvancePanel : MonoBehaviour
 {
     [SerializeField]
