08629d7cb9c484b0d46d3c865306cfa0c9186f93
AaroMKS <aaro.savolainen@helsinki.fi>
2026-03-30 16:29:48 +0300

    Shows placeholder values inprofilepanel and fixed issue with the profilepanel padding

--- Assets/Scripts/ASAProfile.cs
+++ Assets/Scripts/ASAProfile.cs
@@ -8,15 +8,54 @@ public class ASAPRofilePanel : MonoBehaviour
     GameObject levelBarGO;
 
     [SerializeField]
-    GameObject levelTextGO;
+    private TextMeshProUGUI levelTextGO;
+
+    [SerializeField]
+    private TextMeshProUGUI performanceTextGO;
+
+    [SerializeField]
+    private TextMeshProUGUI rankTextGO;
+
+    [SerializeField]
+    private TextMeshProUGUI positionTextGO;
 
     void OnEnable()
     {
         UpdateLevelBar();
+        UpdateText();
     }
 
     public void UpdateLevelBar()
     {
-        levelBarGO.GetComponent<Image>().fillAmount = 0.7f;
+        levelBarGO.GetComponent<Image>().fillAmount = user.percentage;
     }
+
+    public void UpdateText()
+    {
+        string performance_text =
+            $"You are performing better than {100 * user.percentage}% of {user.level} learners";
+        performanceTextGO.text = performance_text;
+        levelTextGO.text = user.level;
+        string rank_text = $"Global rank ({user.level})";
+        rankTextGO.text = rank_text;
+        string position_text = $"{user.global_rank}#";
+        positionTextGO.text = position_text;
+    }
+
+    [System.Serializable]
+    public class Stats
+    {
+        public float percentage;
+        public string level;
+        public int cohort_size;
+        public int global_rank;
+    }
+
+    Stats user = new Stats
+    {
+        percentage = 0.7f,
+        level = "A2",
+        cohort_size = 50,
+        global_rank = 17,
+    };
 }
